6408ae1be57ff3131b748abf3d53bbfe166032bb
Mehrdad <khatami-mehrdad@github.com>
2023-02-15 18:12:59 -0800

    adding optimized and simplify

--- export.py
+++ export.py
@@ -9,10 +9,12 @@ n_head = 8
 model = MultiHeadAttention(n_state, n_head)
 input = torch.zeros(1, n_head, n_state)
 
+f = 'MultiHeadAttention_Sim.onnx'
+
 torch.onnx.export(
     model.cpu(),  # --dynamic only compatible with cpu
     input.cpu(),
-    'MultiHeadAttention.onnx',
+    f,
     verbose=False,
     opset_version=12,
     do_constant_folding=True,
@@ -20,3 +22,27 @@ torch.onnx.export(
     output_names=['wv', 'qk'],
     dynamic_axes=None)
 
+# Checks
+import onnx
+model_onnx = onnx.load(f)  # load onnx model
+onnx.checker.check_model(model_onnx)  # check onnx model
+
+# simplify
+import onnxsim
+
+model_onnx, check = onnxsim.simplify(model_onnx)
+assert check, 'assert check failed'
+onnx.save(model_onnx, f)
+
+# onnxruntime
+import onnxruntime as rt
+
+sess_options = rt.SessionOptions()
+
+# Set graph optimization level
+sess_options.graph_optimization_level = rt.GraphOptimizationLevel.ORT_ENABLE_ALL
+
+# To enable model serialization after graph optimization set this
+sess_options.optimized_model_filepath = "MultiHeadAttention_Sim_Opt.onnx"
+
+session = rt.InferenceSession(f, sess_options)
